0f17563f800945dec57a40803d2e346fdd0873e6
thenewsyria <96256695+thenewsyria@users.noreply.github.com>
2023-02-16 13:20:06 +0300

    Add files via upload

---
+++ 766A_Mahmoud_and_Longest_Uncommon_Subsequence.py
@@ -0,0 +1,8 @@
+arr1=input()
+arr2=input()
+
+if arr1==arr2 and len(arr1)==len(arr1):print(-1)
+if len(arr2)>len(arr1):print(len(arr2))
+if len(arr1)>len(arr2):print(len(arr1))
+if len(arr1)==len(arr2) and arr1!=arr2:
+    print(len(arr1))
